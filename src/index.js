a809fe5e4070004640cf8a2074c0bbc5dfa098ed
moogun <moogunjung@gmail.com>
2018-09-10 20:09:51 +0900

    setup store and rootReducer in the React App

--- src/index.js
+++ src/index.js
@@ -4,5 +4,38 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const initialState = {
+ counter: 0
+}
+
+const rootReducer = (state = initialState, action) => {
+
+ if (action.type === 'INCREMENT') {
+   return {
+     // counter: state.counter + 1
+     ...state,
+     counter: state.counter + 1
+   }
+ } else if (action.type === 'ADD') {
+   return {
+     // counter: state.counter + action.value
+     ...state,
+     counter: state.counter + action.value
+   }
+ }
+
+ return state
+}
+
+const store = createStore(rootReducer)
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
