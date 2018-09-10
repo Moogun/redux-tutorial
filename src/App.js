f68fe0d628f01fd747dc8b14d73eae481648be48
moogun <moogunjung@gmail.com>
2018-09-10 21:04:44 +0900

    added mapStateToProps fn to update state

--- src/App.js
+++ src/App.js
@@ -4,9 +4,34 @@ import './App.css';
 import { connect } from 'react-redux'
 
 class App extends Component {
+  state = {
+    text:''
+  };
 
   showState() {
     return <h4>Counter: {this.props.total.counter}</h4>
+  }
+
+  handleIncrementOne = () => {
+    console.log(1);
+    this.props.onIncrementClick()
+  }
+
+  handleAddTen = () => {
+    console.log(10);
+    this.props.onAddClick()
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({text: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('text', this.state.text);
+    this.props.onAddTodo(this.state.text)
+    this.setState({text: ''})
   }
 
   render() {
@@ -15,6 +40,14 @@ class App extends Component {
       <div style={{marginLeft: '30px'}}>
         <h3>Redux Tutorial</h3>
         {this.showState()}
+        <br/>
+        <button onClick={this.handleIncrementOne}>Increment 1</button>
+        <br/>
+        <button onClick={this.handleAddTen}>Add 10</button>
+
+        <br/>
+        <br/>
+      
       </div>
     );
   }
@@ -26,4 +59,15 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementClick: () => {
+      dispatch({type: 'INCREMENT'})
+    },
+    onAddClick: () => {
+      dispatch({type: 'ADD', value: 10})
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
