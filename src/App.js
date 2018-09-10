c0301afadec7921167ead014e2e4f568ba50d616
moogun <moogunjung@gmail.com>
2018-09-10 20:56:23 +0900

    added an element and fn to show the state in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux'
 
 class App extends Component {
+
+  showState() {
+    return <h4>Counter: {this.props.total.counter}</h4>
+  }
+
   render() {
+    console.log('this.props', this.props);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{marginLeft: '30px'}}>
+        <h3>Redux Tutorial</h3>
+        {this.showState()}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    total: state.counter
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import counter from './counter'
 import addTodo from './addTodo'
 
-
 const rootReducer = combineReducers({
   counter,
   addTodo
